6056dec8c0f73042a303e5b1778de9643a809f1d
Jesuslperezc <jesusleonardoperezcabrera@gmail.com>
2025-11-13 03:30:40 -0400

    Porfa

--- PROYECTO-ENTREGA-2/hospital_v2.cpp
+++ PROYECTO-ENTREGA-2/hospital_v2.cpp
@@ -17,7 +17,25 @@ int main() {
     asegurarArchivo("doctores.bin");
     asegurarArchivo("citas.bin");
     asegurarArchivo("historiales.bin");
-    Hospital* hospital = cargarDatosHospital();
+    // Cargar hospital
+    Hospital hospital{};
+    fstream fh("hospital.bin", ios::binary | ios::in | ios::out);
+    fh.seekg(sizeof(ArchivoHeader), ios::beg);
+    fh.read(reinterpret_cast<char*>(&hospital), sizeof(Hospital));
+    if (!fh.good()) {
+        // Si no hay hospital, inicializarlo
+        strcpy(hospital.nombre, "Hospital Central");
+        hospital.totalPacientesRegistrados = 0;
+        hospital.totalDoctoresRegistrados = 0;
+        hospital.totalCitasAgendadas = 0;
+        hospital.totalConsultasRealizadas = 0;
+        hospital.siguienteIDPaciente = 1;
+        hospital.siguienteIDDoctor = 1;
+        hospital.siguienteIDCita = 1;
+        fh.seekp(sizeof(ArchivoHeader), ios::beg);
+        fh.write(reinterpret_cast<const char*>(&hospital), sizeof(Hospital));
+    }
+    fh.close();
 
 
     int opMenu;
