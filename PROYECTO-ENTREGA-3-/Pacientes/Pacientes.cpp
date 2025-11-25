d7087e0c5d22287e1c0471e7a414ef9e410c5770
Jesuslperezc <jesusleonardoperezcabrera@gmail.com>
2025-11-25 17:49:17 -0400

    Update Pacientes.cpp

--- PROYECTO-ENTREGA-3-/Pacientes/Pacientes.cpp
+++ PROYECTO-ENTREGA-3-/Pacientes/Pacientes.cpp
@@ -55,7 +55,7 @@
         fechaCreacion = time(nullptr);
         fechaModificacion = time(nullptr);
     }
-    bool guardarPaciente(const Paciente& paciente) {
+    bool Paciente::guardarPaciente(const Paciente& paciente) {
         escribirRegistro<Paciente>("pacientes.bin",paciente ,0);
         return true;
     }
@@ -67,19 +67,19 @@
         std::strncpy(this->nombre, nuevoNombre, sizeof(this->nombre) - 1);
         this->nombre[sizeof(this->nombre) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setApellido(const char* nuevoApellido){
         std::strncpy(this->apellido, nuevoApellido, sizeof(this->apellido) - 1);
         this->apellido[sizeof(this->apellido) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setCedula(const char* nuevaCedula){
         std::strncpy(this->cedula, nuevaCedula, sizeof(this->cedula) - 1);
         this->cedula[sizeof(this->cedula) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setEdad(int nuevaEdad){
         if(nuevaEdad < 0){
@@ -88,68 +88,68 @@
         } 
         this->edad = nuevaEdad;
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+       guardarPaciente(*this);
 
     }
     void Paciente::setSexo(char nuevoSexo){
         this->sexo = nuevoSexo;
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setTipoSangre(const char* nuevoTipoSangre){
         std::strncpy(this->tipoSangre, nuevoTipoSangre, sizeof(this->tipoSangre) - 1);
         this->tipoSangre[sizeof(this->tipoSangre) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setTelefono(const char* nuevoTelefono){
         std::strncpy(this->telefono, nuevoTelefono, sizeof(this->telefono) - 1);
         this->telefono[sizeof(this->telefono) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setDireccion(const char* nuevaDireccion){
         std::strncpy(this->direccion, nuevaDireccion, sizeof(this->direccion) - 1);
         this->direccion[sizeof(this->direccion) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setEmail(const char* nuevoEmail){
         std::strncpy(this->email, nuevoEmail, sizeof(this->email) - 1);
         this->email[sizeof(this->email) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setAlergias(const char* nuevasAlergias){
         std::strncpy(this->alergias, nuevasAlergias, sizeof(this->alergias) - 1);
         this->alergias[sizeof(this->alergias) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setObservaciones(const char* nuevasObservaciones){
         std::strncpy(this->observaciones, nuevasObservaciones, sizeof(this->observaciones) - 1);
         this->observaciones[sizeof(this->observaciones) - 1] = '\0';
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setActivo(bool estado){
         this->activo = estado;
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);}
+        guardarPaciente(*this);}
     void Paciente::setEliminado(bool estado){
         this->eliminado = estado;
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::incrementarCantidadConsultas(int cantidad){
         this->cantidadConsultas += cantidad;
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::setPrimerConsultaID(int consultaID){
         this->primerConsultaID = consultaID;
         fechaModificacion = time(nullptr);
-        ::guardarPaciente(*this);
+        guardarPaciente(*this);
     }
     void Paciente::agregarCitaID(int citaID){
 
@@ -159,7 +159,7 @@
                 this->citasIDs[i] = citaID;
                 ++this->cantidadCitas;
                 fechaModificacion = time(nullptr);
-                ::guardarPaciente(*this);
+                guardarPaciente(*this);
                 return;
                 
             }
@@ -172,7 +172,7 @@
                 this->citasIDs[i] = -1;
                 if(this->cantidadCitas > 0) --this->cantidadCitas;
                 fechaModificacion = time(nullptr);
-                ::guardarPaciente(*this);
+                guardarPaciente(*this);
                 return;
                 
             }
